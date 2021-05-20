2737a00f3008723919ffed150491c4b8eb30c9d8
Frank Lyner <frank.lyner@maxbrain.com>
2021-05-20 18:38:32 +0200

    fixed concurrency issues with status metric map

--- server/status.go
+++ server/status.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
+)
+
+const (
+	SnapshotKey = "--snapshot--"
 )
 
 // StatusInformation struct holding all status metrics. Meant to be instantiated once per server
 type StatusInformation struct {
-	Stats map[string]int
-	inc   chan string
+	Stats         map[string]int
+	inc           chan string
+	StatsSnapshot map[string]int
 }
 
 // Metric enum of all available metrics
@@ -18,8 +24,9 @@ type Metric string
 // CreateStatusInfo factory to create a new status info and start thread to process incoming metrics
 func CreateStatusInfo() *StatusInformation {
 	s := StatusInformation{
-		Stats: make(map[string]int),
-		inc:   make(chan string, 10),
+		Stats:         make(map[string]int),
+		StatsSnapshot: make(map[string]int),
+		inc:           make(chan string, 10),
 	}
 	go s.processIncoming()
 	return &s
@@ -27,6 +34,12 @@ func CreateStatusInfo() *StatusInformation {
 
 func (s *StatusInformation) processIncoming() {
 	for m := range s.inc {
+		if m == SnapshotKey {
+			for k, v := range s.Stats {
+				s.StatsSnapshot[k] = v
+			}
+			continue
+		}
 		curVal, exists := s.Stats[m]
 		if exists {
 			s.Stats[m] = curVal + 1
@@ -41,6 +54,16 @@ func (s *StatusInformation) IncrementMetric(m string) {
 	s.inc <- m
 }
 
+func (s *StatusInformation) snapshot() map[string]int {
+	s.inc <- SnapshotKey
+	time.Sleep(10 * time.Millisecond)
+	ss := make(map[string]int)
+	for k, v := range s.StatsSnapshot {
+		ss[k] = v
+	}
+	return ss
+}
+
 // StatusController shows status page
 var StatusController Controller = Controller{
 	Name:      "StatusController",
@@ -49,7 +72,7 @@ var StatusController Controller = Controller{
 	Methods:   []string{"GET"},
 	IsSecured: false,
 	ControllerFunc: func(ctx *Context) {
-		stats := &ctx.StatusInformation.Stats
+		stats := ctx.StatusInformation.snapshot()
 		html := strings.Builder{}
 		html.WriteString("<html>This is a status page. <br/><br/>")
 		html.WriteString(
@@ -61,7 +84,7 @@ var StatusController Controller = Controller{
 					<th>Invokation Count</th>
 				</tr>`)
 		for _, ctr := range ctx.Server.GetControllers() {
-			html.WriteString(fmt.Sprintf("<tr><td>%s</td><td>%+v</td><td>%s</td><td align='center'>%d</td></tr>", ctr.Name, ctr.Methods, ctr.Path, (*stats)[ctr.Metric]))
+			html.WriteString(fmt.Sprintf("<tr><td>%s</td><td>%+v</td><td>%s</td><td align='center'>%d</td></tr>", ctr.Name, ctr.Methods, ctr.Path, stats[ctr.Metric]))
 		}
 		html.WriteString("</table>\n")
 		ctx.SendHTMLResponse(http.StatusOK, []byte(html.String()))
