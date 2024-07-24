70628710ac1889c01105a609ddbc7ebc4ddcd9a5
Frank <frank.lyner@maxbrain.com>
2024-07-24 08:38:17 +0200

    Added wbcr updated to go 1.22 updated dependencies

--- go.mod
+++ go.mod
@@ -1,57 +1,63 @@
 module github.com/franklyner/ssf
 
-go 1.18
+go 1.22
 
 require (
 	github.com/auth0/go-jwt-middleware v1.0.1
-	github.com/couchbase/gocb/v2 v2.7.0
+	github.com/couchbase/gocb/v2 v2.9.1
 	github.com/form3tech-oss/jwt-go v3.2.5+incompatible
-	github.com/google/uuid v1.4.0
+	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
-	github.com/prometheus/client_golang v1.17.0
-	github.com/spf13/viper v1.17.0
-	gorm.io/driver/mysql v1.5.2
-	gorm.io/gorm v1.25.5
+	github.com/prometheus/client_golang v1.19.1
+	github.com/spf13/viper v1.19.0
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.11
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/couchbase/gocbcore/v10 v10.3.0 // indirect
-	github.com/couchbase/gocbcoreps v0.1.0 // indirect
-	github.com/couchbase/goprotostellar v1.0.0 // indirect
-	github.com/couchbaselabs/gocbconnstr/v2 v2.0.0-20230515165046-68b522a21131 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/couchbase/gocbcore/v10 v10.5.1 // indirect
+	github.com/couchbase/gocbcoreps v0.1.3 // indirect
+	github.com/couchbase/goprotostellar v1.0.2 // indirect
+	github.com/couchbaselabs/gocbconnstr/v2 v2.0.0-20240607131231-fb385523de28 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
-	github.com/prometheus/client_model v0.5.0 // indirect
-	github.com/prometheus/common v0.45.0 // indirect
-	github.com/prometheus/procfs v0.12.0 // indirect
-	github.com/sagikazarmark/locafero v0.3.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/sagikazarmark/locafero v0.6.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.10.0 // indirect
-	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.53.0 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.26.0 // indirect
-	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
-	golang.org/x/net v0.18.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
-	google.golang.org/grpc v1.59.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240723171418-e6d459c13d2a // indirect
+	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ wbcr/write_behind_cache_repository.go
@@ -0,0 +1,267 @@
+package wbcr
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/franklyner/ssf/server"
+	"gorm.io/gorm"
+)
+
+var (
+	ErrNotFound = server.JSONErrorResponse{
+		Code:       http.StatusNotFound,
+		Message:    "Entity not found",
+		LogMessage: "Entity not found",
+	}
+
+	ErrAlreadyExists = server.JSONErrorResponse{
+		Code:       http.StatusConflict,
+		Message:    "A value with the given key already exists",
+		LogMessage: "A value with the given key already exists",
+	}
+)
+
+type Keyer[K comparable, V any] interface {
+	SetKey(key K)
+	GetKey() K
+	GetValue() V
+	*V // non-interface type constraint
+}
+
+type WBCacheRepository[K comparable, V any, PT Keyer[K, V]] struct {
+	repo          map[K]V
+	persister     Persister[K, V, PT]
+	hasFetchedAll bool
+}
+
+type Persister[K comparable, V any, PT Keyer[K, V]] interface {
+	Create(ctx *server.Context, value PT) (V, error)
+	Update(ctx *server.Context, value PT) (V, error)
+	Get(ctx *server.Context, key K) (V, error)
+	GetAll(ctx *server.Context) ([]PT, error)
+	Delete(ctx *server.Context, key K) error
+}
+
+type ExtendablePersister[K comparable, V any, PT Keyer[K, V]] struct {
+	IntCreate func(*server.Context, K, PT) error
+	IntUpdate func(*server.Context, K, PT) (V, error)
+	IntGet    func(ctx *server.Context, key K) (V, error)
+	IntGetAll func(ctx *server.Context) ([]PT, error)
+	IntDelete func(ctx *server.Context, key K) error
+}
+
+func CreateWBCacheRepository[K comparable, V any, PT Keyer[K, V]](persister Persister[K, V, PT]) *WBCacheRepository[K, V, PT] {
+	return &WBCacheRepository[K, V, PT]{
+		repo:      make(map[K]V),
+		persister: persister,
+	}
+}
+
+func (wbcr *WBCacheRepository[K, V, PT]) GetAllKnownKeys(ctx *server.Context, ensureAllLoaded bool) (map[K]bool, error) {
+	if ensureAllLoaded {
+		err := wbcr.ensureLoaded(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+	keys := make(map[K]bool)
+	for k := range wbcr.repo {
+		keys[k] = true
+	}
+	return keys, nil
+}
+
+func (wbcr *WBCacheRepository[K, V, PT]) GetByKey(ctx *server.Context, key K) (V, error) {
+	v, found := wbcr.repo[key]
+	var err error
+	if !found {
+		e := new(V)
+		if wbcr.hasFetchedAll {
+			return *e, ErrNotFound
+		}
+		v, err = wbcr.persister.Get(ctx, key)
+		if err != nil {
+			return *e, err
+		}
+		wbcr.repo[key] = v
+	}
+	return v, nil
+}
+
+func (wbcr *WBCacheRepository[K, V, PT]) Insert(ctx *server.Context, value PT) error {
+	key := value.GetKey()
+	_, found := wbcr.repo[key]
+	if found {
+		return ErrAlreadyExists
+	}
+
+	v, err := wbcr.persister.Create(ctx, value)
+	if err != nil {
+		fmt.Errorf("error persisting (key: %v): %w", key, err)
+	}
+	wbcr.repo[key] = v
+	return nil
+}
+
+func (wbcr *WBCacheRepository[K, V, PT]) Update(ctx *server.Context, value PT) error {
+	key := value.GetKey()
+	v, err := wbcr.persister.Update(ctx, value)
+	if err != nil {
+		return fmt.Errorf("error updating %v with persister: %w", key, err)
+	}
+	wbcr.repo[key] = v
+	return nil
+}
+
+func (wbcr *WBCacheRepository[K, V, PT]) Delete(ctx *server.Context, key K) error {
+	_, err := wbcr.persister.Get(ctx, key)
+	found := true
+	if err != nil {
+		if errors.Is(err, ErrNotFound) {
+			found = false
+		} else {
+			return fmt.Errorf("error getting %v for deletion with persister: %w", key, err)
+		}
+	}
+	if found {
+		err = wbcr.persister.Delete(ctx, key)
+		if err != nil && !errors.Is(err, ErrNotFound) {
+			return fmt.Errorf("error deleting %v with persister: %w", key, err)
+		}
+	}
+	delete(wbcr.repo, key)
+	return nil
+}
+
+func (wbcr *WBCacheRepository[K, V, PT]) Search(ctx *server.Context, searcher func(V) bool, ensureLoaded bool) ([]V, error) {
+	if ensureLoaded {
+		err := wbcr.ensureLoaded(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+	ret := []V{}
+	for _, v := range wbcr.repo {
+		if searcher(v) {
+			ret = append(ret, v)
+		}
+	}
+
+	return ret, nil
+}
+
+func (wbcr *WBCacheRepository[K, V, PT]) ensureLoaded(ctx *server.Context) error {
+	if !wbcr.hasFetchedAll {
+		vals, err := wbcr.persister.GetAll(ctx)
+		if err != nil {
+			return fmt.Errorf("error fetching all values from persister: %w", err)
+		}
+		for _, v := range vals {
+			wbcr.repo[v.GetKey()] = v.GetValue()
+		}
+		wbcr.hasFetchedAll = true
+	}
+	return nil
+}
+
+func (e *ExtendablePersister[K, V, PT]) GetAll(ctx *server.Context) ([]PT, error) {
+	if e.IntGetAll == nil {
+		return make([]PT, 0), nil
+	}
+	return e.IntGetAll(ctx)
+}
+
+func (e *ExtendablePersister[K, V, PT]) Create(ctx *server.Context, key K, value PT) error {
+	if e.IntCreate == nil {
+		return nil
+	}
+	return e.IntCreate(ctx, key, value)
+}
+func (e *ExtendablePersister[K, V, PT]) Update(ctx *server.Context, key K, value PT) (V, error) {
+	if e.IntUpdate == nil {
+		return value.GetValue(), nil
+	}
+	return e.IntUpdate(ctx, key, value)
+}
+func (e *ExtendablePersister[K, V, PT]) Get(ctx *server.Context, key K) (V, error) {
+	if e.IntGet == nil {
+		return *new(V), ErrNotFound
+	}
+	return e.IntGet(ctx, key)
+}
+func (e *ExtendablePersister[K, V, PT]) Delete(ctx *server.Context, key K) error {
+	if e.IntDelete == nil {
+		return nil
+	}
+	return e.IntDelete(ctx, key)
+}
+
+type GormPersister[K comparable, V any, PT Keyer[K, V]] struct{}
+
+func (p *GormPersister[K, V, PT]) RunMigration(repository *server.Repository) {
+	val := new(V)
+	err := repository.DB.AutoMigrate(*val)
+	if err != nil {
+		panic(err) // fail fast as this happens on startup
+	}
+}
+
+func (p *GormPersister[K, V, PT]) Create(ctx *server.Context, value PT) (V, error) {
+	db := ctx.Repository.DB
+	res := db.Create(value)
+	if res.Error != nil {
+		e := new(V)
+		if res.Error == gorm.ErrDuplicatedKey {
+			return *e, ErrAlreadyExists
+		}
+		return *e, fmt.Errorf("error while inserting OU Team Mapping (%+v): %w", value, res.Error)
+	}
+	return value.GetValue(), nil
+}
+func (p *GormPersister[K, V, PT]) Update(ctx *server.Context, value PT) (V, error) {
+	db := ctx.Repository.DB
+	empty := new(V)
+	res := db.Save(&value) // Save is an upsert
+	if res.Error != nil {
+		return *empty, fmt.Errorf("error while inserting OU Team Mapping (%+v): %w", value, res.Error)
+	}
+
+	return value.GetValue(), nil
+}
+func (p *GormPersister[K, V, PT]) Get(ctx *server.Context, key K) (V, error) {
+	db := ctx.Repository.DB
+	empty := new(V)
+	value := new(PT)
+	v := *value
+	res := db.First(&v, key)
+
+	if res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return *empty, ErrNotFound
+		}
+		return *empty, fmt.Errorf("couldn't find team mapping after update (%+v)", v)
+	}
+	return v.GetValue(), nil
+}
+func (p *GormPersister[K, V, PT]) GetAll(ctx *server.Context) ([]PT, error) {
+	db := ctx.Repository.DB
+	all := []PT{}
+	res := db.Find(&all)
+	if res.Error != nil {
+		return []PT{}, fmt.Errorf("error retrieving all instances: %w", res.Error)
+	}
+	return all, nil
+}
+func (p *GormPersister[K, V, PT]) Delete(ctx *server.Context, key K) error {
+	db := ctx.Repository.DB
+	v := new(V)
+	res := db.Delete(v, key)
+	if res.Error != nil {
+		if !errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("error deleting record with key %v: %w", key, res.Error)
+		}
+	}
+	return nil
+}
